1f6abc803eedf99d2f0ec19235e24b9f3a782328
Armour GU <779675978@qq.com>
2016-11-04 20:35:20 +0800

    userbl空类添加

---
+++ 体系结构设计/IHOClient/src/bussinesslogic/usebl/MarketerController.java
@@ -0,0 +1,14 @@
+package bussinesslogic.usebl;
+
+import bussinesslogicservice.userblservice.MarketerBLService;
+import vo.MarketerVO;
+
+public class MarketerController implements MarketerBLService {
+
+	@Override
+	public MarketerVO showMarketerData(String marketerID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
